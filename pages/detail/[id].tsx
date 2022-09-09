2c1ed09c23b046f151c0b88347b37e859b185adf
xiaoyao <1065645460@qq.com>
2022-09-09 16:15:19 +0800

    ✨ feat: 初步完成post的详情页

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeOff, HiVolumeUp } from "react-icons/hi";
+import axios from "axios";
 
-const Detail = () => {
-  return <div>Detail</div>;
+import { BASE_URL } from "../../utils";
+import { Video } from "../../types";
+
+interface IProps {
+  postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const router = useRouter();
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  // 应该不用添加post
+  useEffect(() => {
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [post, isVideoMuted]);
+
+  const onVideoClick = () => {
+    if (isPlaying) {
+      videoRef?.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
+  if (!post) return null;
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
+        <div className="opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p className="cursor-pointer " onClick={() => router.back()}>
+            <MdOutlineCancel className="text-white text-[35px] hover:opacity-90" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              onClick={onVideoClick}
+              loop
+              src={post?.video?.asset.url}
+              className=" h-full cursor-pointer"
+            ></video>
+          </div>
+
+          <div className="absolute top-[45%] left-[40%]  cursor-pointer">
+            {!isPlaying && (
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10  cursor-pointer">
+          {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+              <HiVolumeOff className="text-white text-3xl lg:text-4xl" />
+            </button>
+          ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+              <HiVolumeUp className="text-white text-3xl lg:text-4xl" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: res.data },
+  };
 };
 
 export default Detail;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import VideoCard from "../components/VideoCard";
 import NoResults from "../components/NoResults";
 import { Video } from "../types";
 
+import { BASE_URL } from "../utils";
+
 interface IProps {
   videos: Video[];
 }
@@ -22,7 +24,7 @@ const Home = ({ videos }: IProps) => {
 };
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  const { data } = await axios.get(`${BASE_URL}/api/post`);
 
   return {
     props: {
