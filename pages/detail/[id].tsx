df16a379699d5a72c862796d3d6a6ef95b2fecea
xiaoyao <1065645460@qq.com>
2022-09-11 21:39:27 +0800

    ✨ feat: 完成Comment组件基础功能

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -28,6 +28,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const { userProfile }: any = useAuthStore();
+  const [comment, setComment] = useState("");
+  const [isPostingComment, setIsPostingComment] = useState(false);
 
   // 应该不用添加post
   useEffect(() => {
@@ -55,6 +57,23 @@ const Detail = ({ postDetails }: IProps) => {
       });
 
       setPost({ ...post, likes: data.likes });
+    }
+  };
+
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+
+      setPost({ ...post, comments: data.comments });
+      setComment("");
+      setIsPostingComment(false);
     }
   };
 
@@ -133,11 +152,11 @@ const Detail = ({ postDetails }: IProps) => {
             )}
           </div>
           <Comments
-          // comment={comment}
-          // setComment={setComment}
-          // addComment={addComment}
-          // comments={post.comments}
-          // isPostingComment={isPostingComment}
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
           />
         </div>
       </div>
