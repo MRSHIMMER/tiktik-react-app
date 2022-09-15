d9ca247e237f86ad2b7d35ff8023e15f9cbe4356
xiaoyao <1065645460@qq.com>
2022-09-15 15:31:13 +0800

    ✨ feat: 完成Suggested accounts

---
+++ pages/profile/[ids].tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
+
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { IUser } from "../../types";
+import { BASE_URL } from "../../utils";
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
